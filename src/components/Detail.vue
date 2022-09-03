157c13af14764baa96189d5fe6781702876e71c3
jhhan00 <jhhan009@gmail.com>
2022-09-04 01:00:09 +0900

    추가했던 주소들 - 버튼으로 접근가능하게 변경

--- src/components/Detail.vue
+++ src/components/Detail.vue
@@ -4,7 +4,20 @@
     <h4>상세 페이지</h4>
     <h5>{{ blogPosts[$route.params.id].title }}</h5>
     <p>{{ blogPosts[$route.params.id].content }}</p>
-    <router-view></router-view>
+
+    <span>
+      <router-link :to="mainLink">상세페이지 메인</router-link>
+    </span>
+    <span style="margin-left: 10px;">
+      <router-link :to="authorLink">author</router-link>
+    </span>
+    <span style="margin-left: 10px;">
+      <router-link :to="commentLink">comment</router-link>
+    </span>
+
+    <div>
+      <router-view></router-view>
+    </div>
   </div>
 </template>
 
@@ -13,6 +26,13 @@ export default {
     name : 'DetailComponent',
     props : {
         blogPosts : Array,
+    },
+    data() {
+      return {
+        mainLink : "/detail/" + this.$route.params.id,
+        authorLink : "/detail/" + this.$route.params.id + "/author",
+        commentLink : "/detail/" + this.$route.params.id + "/comment",
+      }
     },
 }
 </script>
